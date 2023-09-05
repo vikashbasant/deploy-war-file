3424020cbee7ad16337e94281e0c1f46251d9e02
vikashbasant <basantvikash360@gmail.com>
2023-09-05 18:37:12 +0530

    some changes

--- src/main/java/com/kash/deploywarfile/DeployWarFileApplication.java
+++ src/main/java/com/kash/deploywarfile/DeployWarFileApplication.java
@@ -2,9 +2,10 @@ package com.kash.deploywarfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeployWarFileApplication {
+public class DeployWarFileApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployWarFileApplication.class, args);
